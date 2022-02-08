9a03e2473e2864ea2abea2987384adb5579dc751
peterp772000 <peterp772000@gmail.com>
2022-02-08 19:54:39 +0800

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,21 @@
+
+def buildApp () {
+    echo 'building the application....'
+
+}
+
+def testApp () {
+    echo 'testing the application....'
+
+}
+
+
+def deployApp () {
+    echo 'Deploying the application....'
+
+}
+
+
+
+
+return this
